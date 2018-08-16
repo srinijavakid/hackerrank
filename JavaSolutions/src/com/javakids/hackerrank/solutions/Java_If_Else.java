cda7c1715a0946a00e5bac00194455880c9b8f6a
Srinivasan Elangovan <srininearyou@gmail.com>
2018-08-16 10:56:42 +0530

    intitial commit for two problems

---
+++ JavaSolutions/src/com/javakids/hackerrank/solutions/Java_If_Else.java
@@ -0,0 +1,20 @@
+package com.javakids.hackerrank.solutions;
+
+import java.util.Scanner;
+
+/**
+ * @author srini
+ * 
+ *         Problem: https://www.hackerrank.com/challenges/java-if-else/problem
+ *
+ */
+public class Java_If_Else {
+
+	private static final Scanner scanner = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		int N = scanner.nextInt();
+		scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+		scanner.close();
+	}
+}
---
+++ JavaSolutions/src/com/javakids/hackerrank/solutions/Java_Output_Formatting.java
@@ -0,0 +1,24 @@
+package com.javakids.hackerrank.solutions;
+
+import java.util.Scanner;
+
+/**
+ * @author srini
+ * 
+ *         Problem:https://www.hackerrank.com/challenges/java-output-formatting/problem
+ * *
+ */
+public class Java_Output_Formatting {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("================================");
+		for (int i = 0; i < 3; i++) {
+			String s1 = sc.next();
+			int x = sc.nextInt();
+			// Complete this line
+		}
+		System.out.println("================================");
+
+	}
+}
